7cea93c4304363a4fb63174db2b48ed0f1fd081f
joaocarlos1994 <joao_nad@hotmail.com>
2023-03-16 22:20:25 -0300

    Course

---
+++ src/main/java/br/com/advanced/thread/course/FirstLesson.java
@@ -0,0 +1,9 @@
+package br.com.advanced.thread.course;
+
+public class FirstLesson {
+
+    public static void main(final String... args) throws InterruptedException {
+        System.out.println("This is a main thread");
+        Thread.sleep(30000L);
+    }
+}
